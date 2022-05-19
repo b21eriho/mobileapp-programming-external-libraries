7b7f79a8002e7b3b6aba4b4b6825d4445aa4b1a6
Erik Holmberg <b21eriho@student.his.se>
2022-05-19 17:03:33 +0200

    Implement button to create both toasts

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.externallibraries;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import io.github.muddz.styleabletoast.StyleableToast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StyleableToast.makeText(getApplicationContext(), "Heyo", Toast.LENGTH_LONG, R.style.CustomRedToast).show();
+                StyleableToast.makeText(getApplicationContext(), "hoye", Toast.LENGTH_LONG, R.style.CustomBlueToast).show();
+            }
+        });
     }
 }
